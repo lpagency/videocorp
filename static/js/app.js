394806070de11d2f9559dd29cfc5f6f2a87da067
WayoCarrasco <eduardo.carrasco.nz@gmail.com>
2018-02-02 14:07:26 -0300

    tt

--- static/js/app.js
+++ static/js/app.js
@@ -153,3 +153,24 @@ $(document).ready(function () {
     }
 
 });
+
+$(document).ready(function()
+{
+    var owl = $('.owl-carousel');
+    owl.owlCarousel(
+    {
+        items:6,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true
+    });
+    $('.play').on('click',function()
+    {
+        owl.trigger('play.owl.autoplay',[1000])
+    });
+    $('.stop').on('click',function()
+    {
+        owl.trigger('stop.owl.autoplay')
+    });
+});
